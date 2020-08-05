24dcfe45a274b0c42876edefd2b46a5b68b6ebd1
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-08-05 13:56:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  #setter
+  def name
+    @name 
+  end
+  #getter
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def breed
+    @breed
+  end
+  #getter
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  #setter
+  def name
+    @name 
+  end
+  #getter
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def job
+    @job
+  end
+  #getter
+  def job=(job_name)
+    @job = job_name
+  end
+end
